0647191474fbc5504c1272da23dc8dbdb510f1d9
Kim Ramirez <kim.rc.1066@gmail.com>
2023-05-12 13:12:41 -0600

    feat: api updated

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://kodemia-24g-default-rtdb.firebaseio.com/products/.json'
+//const API_URL = 'https://kodemia-24g-default-rtdb.firebaseio.com/products/.json'
+const API_URL ='https://fakestoreapi.com/products'
 
 let getProducts= async () => {
     let response = await fetch (`${API_URL}`)
